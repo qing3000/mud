5c93d62248c76dcf4f2c662d7016802acd0b6308
JohnZ <qing.zhang@oxinst.com>
2022-09-21 21:48:34 +0100

    Added FFT CNN model picture and script to run 3 methods.

---
+++ step6_run3.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 15 23:27:07 2022
+
+@author: zhangq
+"""
+
+import step6_training_cribs
+
+import step6_training_tiles
+import step6_training_fft
+
+step6_training_cribs.main()
+step6_training_tiles.main()
+step6_training_fft.main()
